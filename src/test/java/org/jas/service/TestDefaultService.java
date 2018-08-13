e89ae2c697e09e5c5ac7bfa6e4a57d2ea0b37b3c
josdem <joseluis.delacruz@gmail.com>
2018-08-13 13:29:32 -0400

    Formatting Apache 2.0 license in model and services

--- src/test/java/org/jas/service/TestDefaultService.java
+++ src/test/java/org/jas/service/TestDefaultService.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2013 Jose Luis De la Cruz Morales joseluis.delacruz@gmail.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package org.jas.service;
 
 import static org.junit.Assert.*;
@@ -25,7 +41,7 @@ public class TestDefaultService {
 	private static final String TRACK_NUMBER_METADATA_TWO = TOTAL_TRACKS;
 	private static final String CD_NUMBER = "1";
 	private static final String TOTAL_CD_NUMBER = "1";
-	
+
 	@InjectMocks
 	private DefaultService defaultService = new DefaultService();
 
@@ -35,30 +51,30 @@ public class TestDefaultService {
 	private Metadata metadata_two;
 	@Mock
 	private MetadataService metadataService;
-	
+
 	private List<Metadata> metadatas = new ArrayList<Metadata>();
-	
+
 	@Before
 	public void setup() throws Exception {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void shouldCompleteTotalTracks() throws Exception {
 		setTracknumberExpectations();
 		when(metadataService.isSameAlbum(metadatas)).thenReturn(true);
-		
+
 		assertTrue(defaultService.isCompletable(metadatas));
 	}
-	
+
 	@Test
 	public void shouldComplete() throws Exception {
 		when(metadataService.isSameAlbum(metadatas)).thenReturn(true);
 		setTracknumberExpectations();
-		
+
 		defaultService.complete(metadatas);
 
-		
+
 		verify(metadata_one).setTotalTracks(TOTAL_TRACKS);
 		verify(metadata_one).setCdNumber(CD_NUMBER);
 		verify(metadata_one).setTotalCds(TOTAL_CD_NUMBER);
@@ -66,7 +82,7 @@ public class TestDefaultService {
 		verify(metadata_two).setCdNumber(CD_NUMBER);
 		verify(metadata_two).setTotalCds(TOTAL_CD_NUMBER);
 	}
-	
+
 	private void setTracknumberExpectations() {
 		when(metadata_one.getTrackNumber()).thenReturn(TRACK_NUMBER_METADATA_ONE);
 		when(metadata_two.getTrackNumber()).thenReturn(TRACK_NUMBER_METADATA_TWO);
@@ -78,14 +94,14 @@ public class TestDefaultService {
 	public void shouldNotCompleteIfSingleTrack() throws Exception {
 		List<Metadata> metadatas = new ArrayList<Metadata>();
 		metadatas.add(metadata_one);
-		
+
 		defaultService.isCompletable(metadatas);
-		
+
 		verify(metadata_one, never()).setTotalTracks(TOTAL_TRACKS);
 		verify(metadata_one, never()).setCdNumber(CD_NUMBER);
 		verify(metadata_one, never()).setTotalCds(TOTAL_CD_NUMBER);
 	}
-	
+
 	@Test
 	public void shouldNotCompleteMetadataWhenNoNecesary() throws Exception {
 		when(metadata_one.getTotalTracks()).thenReturn(TOTAL_TRACKS);
@@ -95,10 +111,10 @@ public class TestDefaultService {
 		when(metadata_one.getTotalCds()).thenReturn(TOTAL_CD_NUMBER);
 		when(metadata_two.getTotalCds()).thenReturn(TOTAL_CD_NUMBER);
 		when(metadataService.isSameAlbum(metadatas)).thenReturn(true);
-		
+
 		assertFalse(defaultService.isCompletable(metadatas));
 	}
-	
+
 	@Test
 	public void shouldNotCompleteWhenNoTracknumber() throws Exception {
 		when(metadata_one.getTotalTracks()).thenReturn(StringUtils.EMPTY);
