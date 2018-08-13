968ba9bc27f4d0f1654e21990953d0b1d02ec753
josdem <joseluis.delacruz@gmail.com>
2018-08-13 11:28:10 -0400

    Formatting Apache 2.0 license in service

--- src/main/java/org/jas/service/MusicBrainzFinder.java
+++ src/main/java/org/jas/service/MusicBrainzFinder.java
@@ -1,3 +1,19 @@
+/*
+   Copyright 2013 Jose Luis De la Cruz Morales joseluis.delacruz@gmail.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package org.jas.service;
 
 import org.jas.model.MusicBrainzTrack;
@@ -5,7 +21,7 @@ import org.jas.model.MusicBrainzTrack;
 import com.slychief.javamusicbrainz.ServerUnavailableException;
 
 public interface MusicBrainzFinder {
-	
+
 	public MusicBrainzTrack getAlbum(String artist, String trackname) throws ServerUnavailableException;
 	public MusicBrainzTrack getByAlbum(String trackname, String album) throws ServerUnavailableException;
 
