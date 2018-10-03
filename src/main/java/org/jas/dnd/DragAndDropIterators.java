d31c2c5a2997476efdfc82cec40fc176345ae05b
josdem <joseluis.delacruz@gmail.com>
2018-10-02 21:32:59 -0400

    #3 Changing log4j by slf4j in drag and drop interators

--- src/main/java/org/jas/dnd/DragAndDropIterators.java
+++ src/main/java/org/jas/dnd/DragAndDropIterators.java
@@ -23,11 +23,11 @@ import java.util.Arrays;
 
 import javax.swing.SwingUtilities;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DragAndDropIterators {
-	private static final Log log = LogFactory.getLog(DragAndDropIterators.class);
+  private static final Logger log = LoggerFactory.getLogger(DragAndDropIterators.class);
 
 	public static DoInListeners<Void, DragOverListener> dragEnter(Point point, DraggedObject draggedObject, boolean validationResult,
 			Container currentFrame) {
