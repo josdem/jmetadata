cf1b3f24d5587872e913084585867ba47213925b
josdem <joseluis.delacruz@gmail.com>
2024-11-16 12:12:04 -0600

    Improving implementation

--- src/main/java/org/jas/collaborator/MetadataCollaborator.java
+++ src/main/java/org/jas/collaborator/MetadataCollaborator.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2013 Jose Luis De la Cruz Morales joseluis.delacruz@gmail.com
+   Copyright 2014 Jose Morales contact@josdem.io
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,108 +16,60 @@
 
 package org.jas.collaborator;
 
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.jas.model.Metadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MetadataCollaborator {
 
-	private List<Metadata> metadatas;
+    private List<Metadata> metadatas;
 
-	public void setMetadatas(List<Metadata> metadatas) {
-		this.metadatas = metadatas;
-	}
+    public void setMetadatas(List<Metadata> metadatas) {
+        this.metadatas = metadatas;
+    }
 
-	public String getArtist() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getArtist() == null && metadatas.get(i-1).getArtist() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getArtist().equals(metadatas.get(i-1).getArtist())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getArtist() : StringUtils.EMPTY;
-	}
+    public String getArtist() {
+        var artists = new ArrayList<>();
+        metadatas.forEach(metadata -> artists.add(metadata.getArtist()));
+        return artists.stream().distinct().count() <= 1 && metadatas.getFirst().getArtist() != null ? metadatas.getFirst().getArtist() : StringUtils.EMPTY;
+    }
 
-	public String getAlbum() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getAlbum() == null && metadatas.get(i-1).getAlbum() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getAlbum().equals(metadatas.get(i-1).getAlbum())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getAlbum() : StringUtils.EMPTY;
-	}
+    public String getAlbum() {
+        var albums = new ArrayList<>();
+        metadatas.forEach(metadata -> albums.add(metadata.getAlbum()));
+        return albums.stream().distinct().count() <= 1 && metadatas.getFirst().getAlbum() != null ? metadatas.getFirst().getAlbum() : StringUtils.EMPTY;
+    }
 
-	public String getGenre() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getGenre() == null && metadatas.get(i-1).getGenre() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getGenre().equals(metadatas.get(i-1).getGenre())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getGenre() : StringUtils.EMPTY;
-	}
+    public String getGenre() {
+        var genres = new ArrayList<>();
+        metadatas.forEach(metadata -> genres.add(metadata.getGenre()));
+        return genres.stream().distinct().count() <= 1 && metadatas.getFirst().getGenre() != null ? metadatas.getFirst().getGenre() : StringUtils.EMPTY;
+    }
 
-	public String getYear() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getYear() == null && metadatas.get(i-1).getYear() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getYear().equals(metadatas.get(i-1).getYear())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getYear() : StringUtils.EMPTY;
-	}
+    public String getYear() {
+        var years = new ArrayList<>();
+        metadatas.forEach(metadata -> years.add(metadata.getYear()));
+        return years.stream().distinct().count() <= 1 && metadatas.getFirst().getYear() != null ? metadatas.getFirst().getYear() : StringUtils.EMPTY;
+    }
 
-	public String getTotalTracks() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getTotalTracks() == null && metadatas.get(i-1).getTotalTracks() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getTotalTracks().equals(metadatas.get(i-1).getTotalTracks())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getTotalTracks() : StringUtils.EMPTY;
-	}
+    public String getTotalTracks() {
+        var tracks = new ArrayList<>();
+        metadatas.forEach(metadata -> tracks.add(metadata.getTotalTracks()));
+        return tracks.stream().distinct().count() <= 1 && metadatas.getFirst().getTotalTracks() != null ? metadatas.getFirst().getTotalTracks() : StringUtils.EMPTY;
+    }
 
-	public String getTotalCds() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getTotalCds() == null && metadatas.get(i-1).getTotalCds() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getTotalCds().equals(metadatas.get(i-1).getTotalCds())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getTotalCds() : StringUtils.EMPTY;
-	}
+    public String getTotalCds() {
+        var cds = new ArrayList<>();
+        metadatas.forEach(metadata -> cds.add(metadata.getTotalCds()));
+        return cds.stream().distinct().count() <= 1 && metadatas.getFirst().getTotalCds() != null ? metadatas.getFirst().getTotalCds() : StringUtils.EMPTY;
+    }
 
-	public String getCdNumber() {
-		boolean isTheSame = true;
-		for (int i=1; i< metadatas.size(); i++) {
-			if (metadatas.get(i).getCdNumber() == null && metadatas.get(i-1).getCdNumber() == null){
-				return StringUtils.EMPTY;
-			}
-			if (!metadatas.get(i).getCdNumber().equals(metadatas.get(i-1).getCdNumber())){
-				isTheSame = false;
-			}
-		}
-		return isTheSame ? metadatas.get(0).getCdNumber() : StringUtils.EMPTY;
-	}
+    public String getCdNumber() {
+        var cdNumber = new ArrayList<>();
+        metadatas.forEach(metadata -> cdNumber.add(metadata.getCdNumber()));
+        return cdNumber.stream().distinct().count() <= 1 && metadatas.getFirst().getCdNumber() != null ? metadatas.getFirst().getCdNumber() : StringUtils.EMPTY;
+    }
 
 }
