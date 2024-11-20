538994ea339b72173f44146b4946f0a2de665ba6
josdem <joseluis.delacruz@gmail.com>
2024-11-20 13:26:38 -0600

    Deleting unused method

--- src/main/java/org/jas/util/Environment.java
+++ src/main/java/org/jas/util/Environment.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2013 Jose Luis De la Cruz Morales joseluis.delacruz@gmail.com
+   Copyright 2014 Jose Morales contact@josdem.io
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -18,12 +18,8 @@ package org.jas.util;
 
 public class Environment {
 
-	public static boolean isLinux(){
-		return System.getProperty("os.name").toLowerCase().contains("linux");
-	}
-
-	public static boolean isWindows(){
-		return System.getProperty("os.name").toLowerCase().contains("windows");
-	}
+    public static boolean isLinux() {
+        return System.getProperty("os.name").toLowerCase().contains("linux");
+    }
 
 }
