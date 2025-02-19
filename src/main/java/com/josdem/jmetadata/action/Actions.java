7c403a1182694f40773e96406c000a9698cbc96b
Jonathan Narvaez Sosa <jonathan.narvaez@encora.com>
2025-02-18 23:56:37 -0600

    Move constants from interface Actions to final class

--- src/main/java/com/josdem/jmetadata/action/Actions.java
+++ src/main/java/com/josdem/jmetadata/action/Actions.java
@@ -27,39 +27,39 @@ import org.asmatron.messengine.action.EmptyAction;
 import org.asmatron.messengine.action.RequestAction;
 import org.asmatron.messengine.action.ValueAction;
 
-/**
- * @author josdem (joseluis.delacruz@gmail.com)
- * @understands A class who has Publisher Subscriber label descriptors
- */
-public interface Actions {
-  String LOGIN_ID = "login";
-  ActionId<ValueAction<User>> LOGIN = cm(LOGIN_ID);
+public final class Actions {
 
-  String GET_METADATA = "getMetadata";
-  ActionId<EmptyAction> METADATA = cm(GET_METADATA);
+  private Actions() {}
 
-  String SEND_METADATA = "sendMetadata";
-  ActionId<RequestAction<Metadata, ActionResult>> SEND = cm(SEND_METADATA);
+  public static final String LOGIN_ID = "login";
+  public static final ActionId<ValueAction<User>> LOGIN = cm(LOGIN_ID);
 
-  String COMPLETE_ALBUM_METADATA = "completeAlbumMetadata";
-  ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_MUSICBRAINZ =
+  public static final String GET_METADATA = "getMetadata";
+  public static final ActionId<EmptyAction> METADATA = cm(GET_METADATA);
+
+  public static final String SEND_METADATA = "sendMetadata";
+  public static final ActionId<RequestAction<Metadata, ActionResult>> SEND = cm(SEND_METADATA);
+
+  public static final String COMPLETE_ALBUM_METADATA = "completeAlbumMetadata";
+  public static final ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_MUSICBRAINZ =
       cm(COMPLETE_ALBUM_METADATA);
 
-  String COMPLETE_LAST_FM_METADATA = "completeLastfmMetadata";
-  ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_LAST_FM =
+  public static final String COMPLETE_LAST_FM_METADATA = "completeLastfmMetadata";
+  public static final ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_LAST_FM =
       cm(COMPLETE_LAST_FM_METADATA);
 
-  String WRITE_METADATA = "writeMetadata";
-  ActionId<RequestAction<Metadata, ActionResult>> WRITE = cm(WRITE_METADATA);
+  public static final String WRITE_METADATA = "writeMetadata";
+  public static final ActionId<RequestAction<Metadata, ActionResult>> WRITE = cm(WRITE_METADATA);
 
-  String EXPORT_METADATA = "exportMetadata";
-  ActionId<RequestAction<ExportPackage, ActionResult>> EXPORT = cm(EXPORT_METADATA);
+  public static final String EXPORT_METADATA = "exportMetadata";
+  public static final ActionId<RequestAction<ExportPackage, ActionResult>> EXPORT =
+      cm(EXPORT_METADATA);
 
-  String COMPLETE_FORMATTER_METADATA = "completeFormatterMetadata";
-  ActionId<RequestAction<Metadata, ActionResult>> COMPLETE_FORMATTER =
+  public static final String COMPLETE_FORMATTER_METADATA = "completeFormatterMetadata";
+  public static final ActionId<RequestAction<Metadata, ActionResult>> COMPLETE_FORMATTER =
       cm(COMPLETE_FORMATTER_METADATA);
 
-  String COMPLETE_DEFAULT_METADATA = "completeDefaultMetadata";
-  ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_DEFAULT =
+  public static final String COMPLETE_DEFAULT_METADATA = "completeDefaultMetadata";
+  public static final ActionId<RequestAction<List<Metadata>, ActionResult>> COMPLETE_DEFAULT =
       cm(COMPLETE_DEFAULT_METADATA);
 }
