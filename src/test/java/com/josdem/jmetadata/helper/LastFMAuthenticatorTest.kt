8625e8237f09fc0f1f63eef80bf39064c7bca7fa
josdem <joseluis.delacruz@gmail.com>
2025-04-21 11:53:48 -0600

    #118 Translating lastFM authenticator missing test to kotlin

--- src/test/java/com/josdem/jmetadata/helper/LastFMAuthenticatorTest.kt
+++ src/test/java/com/josdem/jmetadata/helper/LastFMAuthenticatorTest.kt
@@ -17,12 +17,18 @@
 package com.josdem.jmetadata.helper
 
 import de.umass.lastfm.Session
+import org.apache.commons.lang3.StringUtils
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import org.slf4j.LoggerFactory
+
+private const val USERNAME = "josdem"
 
 internal class LastFMAuthenticatorTest {
     private lateinit var lastFMAuthenticator: LastFMAuthenticator
@@ -31,6 +37,8 @@ internal class LastFMAuthenticatorTest {
 
     @Mock private lateinit var session: Session
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     @BeforeEach
     fun setup() {
         MockitoAnnotations.openMocks(this)
@@ -38,10 +46,27 @@ internal class LastFMAuthenticatorTest {
     }
 
     @Test
-    fun `should login`() {
-        val username = "josdem"
+    fun `should login`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val username = USERNAME
         val password = "password"
         `when`(authenticatorHelper.getSession(username, password)).thenReturn(session)
         assertNotNull(lastFMAuthenticator.login(username, password)) { "should login" }
+    }
+
+    @Test
+    fun `should fail to login due to not username nor password`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val username = StringUtils.EMPTY
+        val password = StringUtils.EMPTY
+        assertNull(lastFMAuthenticator.login(username, password)) { "should fail to login due to not username nor password" }
+    }
+
+    @Test
+    fun `should fail to login`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val username = USERNAME
+        val password = "invalidPassword"
+        assertNull(lastFMAuthenticator.login(username, password)) { "should fail to login due to invalid credentials" }
     }
 }
