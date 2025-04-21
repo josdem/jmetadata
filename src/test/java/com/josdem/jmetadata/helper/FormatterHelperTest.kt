5f1a2f5658e5545d4ffd9ea725e850a08e790079
josdem <joseluis.delacruz@gmail.com>
2025-04-21 15:00:48 -0600

    #118 Translating formatter helper test to kotlin

---
+++ src/test/java/com/josdem/jmetadata/helper/FormatterHelperTest.kt
@@ -0,0 +1,37 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.josdem.jmetadata.helper
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.slf4j.LoggerFactory
+
+internal class FormatterHelperTest {
+    private val formatterHelper: FormatterHelper = FormatterHelper()
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `should format keyword deleting special characters`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val word = "de - pazz"
+        val expectedWord = "depazz"
+
+        assertEquals(expectedWord, formatterHelper.getBasicFormat(word))
+    }
+}
